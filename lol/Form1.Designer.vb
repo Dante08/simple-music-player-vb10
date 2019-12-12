14650b19bd01b9ae817e37b1e577396d9da4ef80
wandapcguide <wandatamvan4@gmail.com>
2019-12-12 11:46:46 +0700

    initial commit

---
+++ lol/Form1.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(25, 22)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(294, 234)
+        Me.AxWindowsMediaPlayer1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources._default
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button1.Location = New System.Drawing.Point(297, 276)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 34)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "ADD PLAYLIST"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources._default
+        Me.Button2.Location = New System.Drawing.Point(118, 370)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(51, 34)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "PLAY"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources._default
+        Me.Button3.Location = New System.Drawing.Point(188, 370)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(51, 34)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "PAUSE"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources._default
+        Me.Button4.Location = New System.Drawing.Point(268, 370)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(51, 34)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "STOP"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources._default
+        Me.Button5.Location = New System.Drawing.Point(340, 370)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(51, 34)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "PREV"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources._default
+        Me.Button6.Location = New System.Drawing.Point(415, 370)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(51, 34)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "NEXT"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(25, 284)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(239, 20)
+        Me.TextBox1.TabIndex = 10
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BackColor = System.Drawing.Color.Bisque
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(25, 22)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(459, 238)
+        Me.ListBox1.TabIndex = 11
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(25, 324)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(239, 20)
+        Me.TextBox2.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources._default
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(518, 433)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+
+End Class
---
+++ lol/Form1.vb
@@ -0,0 +1,88 @@
+﻿Public Class Form1
+    Dim currentTrack As Integer
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath.ToString
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If (Not ListBox1.SelectedItem = Nothing) Then
+            AxWindowsMediaPlayer1.URL = TextBox1.Text & "\" & ListBox1.SelectedItem
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.pause()
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.stop()
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        If currentTrack = 0 Then
+            AxWindowsMediaPlayer1.URL = ListBox1.Items(ListBox1.Items.Count - 1)
+            currentTrack = ListBox1.Items.Count - 1
+        Else
+            AxWindowsMediaPlayer1.URL = ListBox1.Items(currentTrack - 1)
+            currentTrack -= 1
+        End If
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        If currentTrack = (ListBox1.Items.Count = 1) Then
+            AxWindowsMediaPlayer1.URL = ListBox1.Items(0)
+        Else
+            AxWindowsMediaPlayer1.URL = ListBox1.Items(currentTrack + 1)
+            currentTrack += 1
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        ListBox1.Items.Clear()
+        If Not TextBox1.Text = "" Then
+            For Each File As String In My.Computer.FileSystem.GetFiles(TextBox1.Text, Microsoft.VisualBasic.FileIO.SearchOption.SearchAllSubDirectories, "*mp3")
+                ListBox1.Items.Add(File)
+            Next
+        End If
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ListBox1.SelectedIndexChanged
+        AxWindowsMediaPlayer1.URL = ListBox1.SelectedItem.ToString
+        currentTrack = ListBox1.SelectedIndex
+
+    End Sub
+
+    Private Sub Button5_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Button5.KeyDown
+        If e.KeyCode = Keys.F1 Then
+            If currentTrack = 0 Then
+                AxWindowsMediaPlayer1.URL = ListBox1.Items(ListBox1.Items.Count - 1)
+                currentTrack = ListBox1.Items.Count - 1
+            Else
+                AxWindowsMediaPlayer1.URL = ListBox1.Items(currentTrack - 1)
+                currentTrack -= 1
+            End If
+        End If
+    End Sub
+
+    Private Sub Button6_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Button6.KeyDown
+        If e.KeyCode = Keys.F2 Then
+            If currentTrack = (ListBox1.Items.Count = 1) Then
+                AxWindowsMediaPlayer1.URL = ListBox1.Items(0)
+            Else
+                AxWindowsMediaPlayer1.URL = ListBox1.Items(currentTrack + 1)
+                currentTrack += 1
+            End If
+        End If
+    End Sub
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+        TextBox2.Text = AxWindowsMediaPlayer1.currentMedia.getItemInfo("Artist") & " - " & AxWindowsMediaPlayer1.currentMedia.getItemInfo("Title")
+    End Sub
+End Class
